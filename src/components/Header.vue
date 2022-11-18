ae4ffe388af08990526618e078cfb0ab5ac3aa08
Nicole <2niico13@gmail.com>
2022-11-18 16:26:50 -0500

    Added basic functionality. click button to display <p>; content of <p> is reactive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,19 @@
-<script>
+<script setup>
 import { ref } from "vue";
-// const list = ref("");
-const moviesList = ; //left off herer
+
 const clicked = ref(false);
-const 
+const selected = ref("");
 
-const getMovie = () => {
+const getData = () => {
   clicked.value = true;
-
 }
 </script>
 
 <template>
   <div class="list-container">
     <label for="movies-list">Choose a movie to view its information:</label><br>
-    <select id="movies-list" >
-      <option value="" selected>--Choose a movie--</option>
+    <select id="movies-list" v-model="selected">
+      <option value="0" disabled>Choose a movie</option>
       <option value="11216">Cinema Paradiso</option>
       <option value="13">Forrest Gump</option>
       <option value="483855">Papers, Please: The Short Film</option>
@@ -27,11 +25,10 @@ const getMovie = () => {
       <option value="1402">The Pursuit of Happyness</option>
       <option value="372058">Your Name</option>
     </select>
-    <button id="get" @click="getMovie()">Get</button>
+    <button id="get" @click="getData()">Get</button>
   </div>
-  <div v-if="clicked" class="print-id">
-    <p>{{ list }}</p>
-  </div>
+  <p v-if="clicked">{{ selected }}</p>
+
 </template>
 
 <style scoped>
